df6d2ec63e48585505dd7540e621aff6c51852e5
Abdul Basith <abdulbasith.eee@hotmail.com>
2023-12-06 05:09:28 +0530

    seperate config into many files part 1

--- init.lua
+++ init.lua
@@ -146,9 +146,9 @@ require('lazy').setup({
   {
     -- Theme inspired by Atom
     'navarasu/onedark.nvim',
-    priority = 1000,
+    priority = 10000,
     config = function()
-      vim.cmd.colorscheme 'sorbet'
+      vim.cmd.colorscheme 'onedark'
     end,
   },
 
@@ -227,79 +227,8 @@ require('lazy').setup({
 -- See `:help vim.o`
 -- NOTE: You can change these options as you wish!
 
--- Set highlight on search
-vim.o.hlsearch = false
-
-vim.o.ignorecase = true
-
-vim.o.termguicolors = true
-
-vim.wo.number = true
-vim.o.relativenumber = true
-
-vim.o.wrap = false
-vim.o.expandtab = true
-vim.o.tabstop = 3
-vim.o.shiftwidth = vim.o.tabstop
-
-vim.o.scrolloff = 8
-
-vim.o.clipboard = "unnamedplus"
-
--- When new window opens put it below the available window
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-
-vim.o.inccommand = "split"
-
--- Enable mouse mode
-vim.o.mouse = 'a'
-
-vim.o.virtualedit = "block"
-
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
-
--- Enable break indent
-vim.o.breakindent = true
-
--- Save undo history
-vim.o.undofile = true
-
--- Case-insensitive searching UNLESS \C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
-
--- Decrease update time
-vim.o.updatetime = 250
-vim.o.timeoutlen = 300
-
--- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
-
--- NOTE: You should make sure your terminal supports this
-vim.o.termguicolors = true
-
--- [[ Basic Keymaps ]]
-
--- Keymaps for better default experience
--- See `:help vim.keymap.set()`
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-
--- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
--- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+require("lua.custom.plugins.options")
+require("lua.custom.plugins.keymaps")
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
